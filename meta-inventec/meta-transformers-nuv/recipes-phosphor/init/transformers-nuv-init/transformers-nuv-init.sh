72392b750e5bffd9e6b7d32fad13e98423d791a0
Tommy Lin <lin.tommysc@inventec.com>
2022-11-17 17:55:17 +0800

    Bug 1347 - [SW][Common] Telemetry Service - Log Configuration
    Symptom/Reason:
    - MS requirement
      - M-BMC-MAN-0080
      - M-BMC-MAN-0081
      - M-BMC-MAN-0082
      - M-BMC-MAN-0084
      - M-BMC-MAN-0085
      - M-BMC-MAN-0086
    
    Root Cause:
    - N/A
    
    Solution/Change:
    - [rsyslog]
      - Add log template IPMICmdTemplate
      - Add new configurations for log rotation
    - [transformers-init]
      - Add symbolic link for /var/log under /var/wcs/home
    - [transformers-nuv-init]
      - Add symbolic link for /var/log under /var/wcs/home
    
    Entry Test:
    - N/A

--- meta-inventec/meta-transformers-nuv/recipes-phosphor/init/transformers-nuv-init/transformers-nuv-init.sh
+++ meta-inventec/meta-transformers-nuv/recipes-phosphor/init/transformers-nuv-init/transformers-nuv-init.sh
@@ -21,3 +21,9 @@ bash $EMMC_CHECK_SH
 # run cpld verion
 CPLD_CHECK="/usr/sbin/transformers-nuv-cpld-init.sh"
 bash $CPLD_CHECK
+
+if [ ! -d "/var/wcs/home/log" ]
+then
+    ln -s /var/log /var/wcs/home/
+    echo "Create soft link /var/wcs/home/log"
+fi
--- meta-inventec/meta-transformers/recipes-phosphor/init/transformers-init/transformers-post-init.sh
+++ meta-inventec/meta-transformers/recipes-phosphor/init/transformers-init/transformers-post-init.sh
@@ -4,3 +4,9 @@ gpioset `gpiofind BMC_READY`=0
 echo BMC ready !!
 gpioset `gpiofind RST_BMC_SGPIO`=1
 echo Release reset SGPIO !!
+
+if [ ! -d "/var/wcs/home/log" ]
+then
+    ln -s /var/log /var/wcs/home/
+    echo "Create soft link /var/wcs/home/log"
+fi
